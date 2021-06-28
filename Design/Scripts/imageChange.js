382d6213843863b3f9b9eb1cc51a12a78e28d31f
Cinemato <alharerekhaled@gmail.com>
2021-06-28 20:34:47 +0300

    Full Website Design

---
+++ Design/Scripts/imageChange.js
@@ -0,0 +1,20 @@
+//Product Front Image Change Script
+
+var productImg = document.getElementById('product-image');
+var smallImgs = document.getElementsByClassName('small-image');
+
+for(let i = 0; i < smallImgs.length; i++)
+{
+    smallImgs[i].onclick = function()
+    {
+        for(let j = 0; j < smallImgs.length; j++)
+        {
+            smallImgs[j].parentNode.style.borderColor = "#E7E7E7";
+        }
+        
+        productImg.src = smallImgs[i].src;
+        smallImgs[i].parentNode.style.borderColor = "#FFA20C";
+    }
+}
+
+//-------------------- Done
---
+++ Design/Scripts/mobileMenu.js
@@ -0,0 +1,16 @@
+var menuItems = document.getElementsByClassName("mobile")[0];
+var burger = document.getElementById("burger");
+
+menuItems.style.maxHeight = "0px";
+
+function menutoggle(){
+    if(menuItems.style.maxHeight == "0px"){
+        menuItems.style.maxHeight = "300px";
+        burger.src = "Images/x.svg";
+    }
+    
+    else{
+        menuItems.style.maxHeight = "0px";
+        burger.src = "Images/menu.svg";
+    }
+}
